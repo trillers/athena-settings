de3ad774dbb3eef24f5e010c253689bb7c587b6e
leekangtaqi <leekangtaqi@sina.com>
2015-08-10 18:15:46 +0800

    ok

---
+++ index.js
@@ -0,0 +1,76 @@
+var settings = module.exports = {
+  env: {
+    name: 'dev',
+    mode: 'development',
+    host: '127.0.0.1',
+    port: 3020,
+    bindIp: "0.0.0.0",
+
+    NODE_ENV: 'development',
+    PORT: 3020,
+    BINDIP: "0.0.0.0"
+  },
+
+  secretKey: 'quick',
+
+  app: {
+    protocol: 'http',
+    host: '127.0.0.1',
+    port: 3020,
+    domain: 'http://ci.bot.bao.nong600.com',
+    domainPort: 80,
+    context: '/'
+  },
+
+  api: {
+    url: 'http://ci.bot.bao.nong600.com/api'
+  },
+
+  logging: {
+    reloadSecs: 0, //INFO: set 0 could let nodeunit tests which use log4js exit properly
+    level: 'DEBUG'
+  },
+
+  redis:{
+    mode:'single',
+    host: '127.0.0.1',
+    port: 6379,
+    auth:'',
+    sentinel: {
+      hosts:[{host: '127.0.0.1', port: 26379}],
+      masterName:'mymaster'
+    }
+  },
+
+  mongo: {
+    host: "127.0.0.1",
+    port: 27017,
+    db: "zeus",
+    username: '',
+    password: ''
+  },
+
+  session: {
+    secretKey: 'quick',
+    expires: 60 // minutes
+  },
+
+  wechat: {
+    appKey: 'wxf93b94a5d4c9beb0',
+    appSecret: '108547cde3d34b636d590185a8345a58',
+    token: 'trillers',
+    encodingAESKey: 'u66H0iFUqhQNCUMV4zAwMpy3fIFDR7a3GLSAm1rVz7d'
+  },
+
+  locationServer: {
+    host: 'api.map.baidu.com',
+    port: 80,
+    ak: 'PwCZ3FQOyXeHDQKRQZvsrL8k'
+  },
+  
+  resources: {
+    app: {
+      name: "mit"
+    }
+  }
+};

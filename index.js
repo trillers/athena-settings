f42f40ef65e0adb37620504af6affd74019b06b8
leekangtaqi <leekangtaqi@sina.com>
2015-08-18 10:25:29 +0800

    test

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ var settings = module.exports = {
     protocol: 'http',
     host: '127.0.0.1',
     port: 3020,
-    domain: 'http://ci.bot.bao.nong600.com',
+    domain: 'http://ci.www.wenode.org',
     domainPort: 80,
     context: '/'
   },
 
   api: {
-    url: 'http://ci.bot.bao.nong600.com/api'
+    url: 'http://ci.www.wenode.org/api'
   },
 
   logging: {

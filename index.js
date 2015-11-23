97f347485ad910b84c6197dd7b9ceba40754f40b
henryleu <henryleu@126.com>
2015-11-23 17:16:34 +0800

    change mongo settings

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ var settings = module.exports = {
   mongo: {
     host: "127.0.0.1",
     port: 27017,
-    db: "trillers",
-    username: 'trillers',
-    password: 'trillers'
+    db: "wenode",
+    username: 'wenode',
+    password: 'wenode'
   },
 
   session: {

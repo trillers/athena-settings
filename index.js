187690f9471cac353bed2e82a2854a1dba1a67d0
node-rookie <749924911@qq.com>
2015-11-27 13:53:02 +0800

    update

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ var settings = module.exports = {
   rabbitmq: {
     host: "114.215.91.211",
     port: 5672,
-    vhost: 'ci',
+    vhost: 'wenode_ci',
     username: 'wenode',
     password: 'wen0dep@ssw0rd!'
   },

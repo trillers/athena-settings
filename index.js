1b98e0002bcceb678220d72aa84f0bcd4fe82eaf
henryleu <henryleu@126.com>
2015-12-08 21:16:58 +0800

    change to case.www.wenode.org

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ var settings = module.exports = {
     protocol: 'http',
     host: '127.0.0.1',
     port: 3020,
-    domain: 'http://dev.www.wenode.org',
+    domain: 'http://case.www.wenode.org',
     domainPort: 80,
     context: '/'
   },
 
   api: {
-    url: 'http://dev.www.wenode.org/api'
+    url: 'http://case.www.wenode.org/api'
   },
 
   logging: {
